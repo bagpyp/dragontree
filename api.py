9680b0ba43acecd0674488daeac15c73436034fc
bagpyp <robert@bagpyp.net>
2020-10-25 16:08:28 -0700

    host and port 2

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,4 +13,5 @@ def add():
     return jsonify(float(a) + float(b))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
