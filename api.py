d23b1ed16dcda156c1ea05fe9b4172972397b789
bagpyp <robert@bagpyp.net>
2020-10-28 11:31:17 -0700

    access db_url thru heroku config

--- api.py
+++ api.py
@@ -6,18 +6,21 @@ import sqlalchemy
 
 app = Flask(__name__)
 
+conf = os.popen('heroku config').read()
+url = 'postgres' + conf.split('postgres')[1]
+
 @app.route('/record')
 def collect_measurement():
     dragontree = int(request.args.get('dragontree'))
     now = time.strftime('%Y-%m-%d %H:%M:%S')
-    engine = sqlalchemy.create_engine('postgres://ynumvhqilbxvod:590b20a0b406d7b78c825d36151de44e5bd1ecbc53529c0db3c4f50685443093@ec2-3-216-92-193.compute-1.amazonaws.com:5432/d8keu230b7fs9s')
+    engine = sqlalchemy.create_engine(url)
     engine.connect()
     engine.execute(f"INSERT INTO dragontree VALUES ('{now}',{dragontree})")
     return f"Inserted moisture value {dragontree} in table 'dragontree' at time {now}"
 
 @app.route('/')
 def display_data():
-    engine = sqlalchemy.create_engine('postgres://ynumvhqilbxvod:590b20a0b406d7b78c825d36151de44e5bd1ecbc53529c0db3c4f50685443093@ec2-3-216-92-193.compute-1.amazonaws.com:5432/d8keu230b7fs9s')
+    engine = sqlalchemy.create_engine(url)
     engine.connect()
     data = engine.execute('SELECT * FROM dragontree')
     sheet = ''.join([str(row)+'\n' for row in data])
@@ -26,3 +29,4 @@ def display_data():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
